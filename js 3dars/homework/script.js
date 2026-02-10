9c1d65a7c9468406d5d3e6a4a506623340987e35
ilxomovv <aaalll00110011100@gmail.com>
2026-02-10 14:46:46 +0500

    yangirepo

--- js 3dars/homework/script.js
+++ js 3dars/homework/script.js
@@ -2,16 +2,22 @@ var ism = prompt("Ismingizni kiriting").trim();
 console.log("salom" + " " + `${ism}` + " " + "yaxshimisiz");
 if(ism==""){
     alert("ismingizni kiriting!")
-
+    var ism = prompt("Ismingizni kiriting").trim();
+    console.log("ismingizni kiritmagansiz");
+    
 }else if (ism.length <= 3) {
     alert("ism juda kam")
 
-    
+    var ism = prompt("Ismingizni kiriting").trim();
 }
 var nimaga = prompt("nima uchun institutga kirishni xohlaysiz")
 
 if(nimaga.length <=10) {
     alert("Siz kam malumot kiritdingiz, 10ta harfdan ko'p bo'lsin")
+    
+
+    console.log("siz nima uchun institutga kirishni xohganingizni yozmadingiz");
+    
 }else{
     alert("raxmat😉")
 }
