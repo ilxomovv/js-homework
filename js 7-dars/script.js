acb5e1eb02982c6b2f60b6c1c7962867e5f0281e
ilxomovv <aaalll00110011100@gmail.com>
2026-02-18 18:36:40 +0500

    kalkulyator mukammali

---
+++ js 7-dars/script.js
@@ -0,0 +1,21 @@
+var son1 = +prompt("1-sonni kirirting").trim();
+var result
+
+
+switch(amal){
+    case (`-`):
+        alert(son1 - son2);
+        break;
+    case (`+`):
+        alert(son1 + son2);
+        break;
+    case (`/`):
+        alert(son1 / son2);
+        break;
+    case (`*`):
+        alert(son1 * son2);
+        break;
+
+    default:
+        alert(`2-promntga kisdasdasdad`)
+}
