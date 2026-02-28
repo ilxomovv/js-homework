dda6dd780818a19cb80ef5c45411ea80936712ef
ilxomovv <aaalll00110011100@gmail.com>
2026-02-28 15:24:29 +0500

    ooo

---
+++ js 10-dars/homework/script.js
@@ -0,0 +1,10 @@
+let soz = prompt("Biror so'z kiriting:");
+let teskariSoz = "";
+
+
+for (let i = soz.length - 1; i >= 0; i--) {
+    teskariSoz += soz[i];
+}
+
+alert("Chappasiga: " + teskariSoz);
+ 
